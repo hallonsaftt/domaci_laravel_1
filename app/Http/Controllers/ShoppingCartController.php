ae089e6a6bcfb83b211112973844c80557547baa
hallonsaftt <hallonsaftt@gmail.com>
2025-04-08 18:31:51 +0200

    #Domaci Cart Fix

--- app/Http/Controllers/ShoppingCartController.php
+++ app/Http/Controllers/ShoppingCartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CartAddRequest;
+use App\Models\ShopModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Routing\Controller;
@@ -12,14 +13,31 @@ class ShoppingCartController extends Controller
     public function cart()
     {
 
+        $allProducts = [];
+
+        foreach (Session::get('product') as $cartItem)
+        {
+            $allProducts[] = $cartItem['product_id'];
+        }
+
+        $products = ShopModel::whereIn('id', $allProducts)->get();
+
         return view('cart', [
-            'cart' => Session::get('product')
+            'cart' => Session::get('product'),
+            'products' => $products,
         ]);
     }
 
 
     public function addToCart(CartAddRequest $request)
     {
+        $product = ShopModel::where(['id' => $request->id])->first();
+
+        if($product->amount < $request->amount)
+        {
+            return redirect()->back();
+        }
+
         Session::push('product', [
             'product_id' => $request->id,
             'amount' => $request->amount,
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -8,13 +8,12 @@
     <div class="container my-4">
         <h3 class="mb-4">🛒 Your Cart</h3>
 
-        @foreach($cart as $product)
-            @if(is_array($product) && isset($product['product_id'], $product['amount']))
-                <p>Product ID: {{ $product['product_id'] }}</p>
-                <p>Quantity: {{ $product['amount'] }}</p>
-            @else
-                <p style="color:red;">⚠️ Neispravan unos: {{ json_encode($product) }}</p>
-            @endif
+        @foreach($products as $product)
+
+            <div class="cart-conatiner">
+                    <span>{{ $product->name }}</span>
+                    <span>{{ $product->amount }}</span>
+                </div>
         @endforeach
 
 
